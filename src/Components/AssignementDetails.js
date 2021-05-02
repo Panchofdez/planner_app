9c403a193c86dd161b5c848fc4acd43e254a0f22
Pancho Fernandez <panchofern@gmail.com>
2021-05-02 04:06:41 -0400

    updated assignmentcreation screen

--- src/Components/AssignementDetails.js
+++ src/Components/AssignementDetails.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, Text, Dimensions } from "react-native";
+import { View, Text, Dimensions, StyleSheet } from "react-native";
+import { Input, Button } from "react-native-elements";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -10,8 +12,118 @@ const AssignementDetails = () => {
       style={{
         backgroundColor: "white",
         width: windowWidth * 0.9,
+        borderRadius: 10,
       }}
-    ></View>
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 10,
+        }}
+      >
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: "#F0F0F0",
+            width: "60%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: 10,
+            height: 100,
+            borderRadius: 8,
+            backgroundColor: "#F0F0F0",
+          }}
+        >
+          <Input
+            placeholder="Description"
+            inputContainerStyle={{ borderColor: "transparent" }}
+          />
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            width: "37%",
+          }}
+        >
+          <View
+            style={{
+              borderWidth: 1,
+              borderColor: "#F0F0F0",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: 5,
+              borderRadius: 8,
+              height: 45,
+              backgroundColor: "#F0F0F0",
+            }}
+          >
+            <Input
+              placeholder="Due Date"
+              inputContainerStyle={{
+                borderColor: "transparent",
+              }}
+            />
+          </View>
+          <View
+            style={{
+              borderWidth: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 5,
+              height: 45,
+              borderRadius: 8,
+              backgroundColor: "#F0F0F0",
+              borderColor: "#F0F0F0",
+            }}
+          >
+            <Input
+              placeholder="Weight"
+              inputContainerStyle={{ borderColor: "transparent" }}
+            />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          height: 100,
+          display: "flex",
+          padding: 10,
+          paddingTop: 5,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "#F0F0F0",
+            flex: 1,
+            borderRadius: 8,
+            backgroundColor: "#F0F0F0",
+          }}
+        >
+          <Input
+            placeholder="Difficulty"
+            inputContainerStyle={{ borderColor: "transparent" }}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignContent: "center",
+            }}
+          ></View>
+        </View>
+      </View>
+    </View>
   );
 };
 
--- src/screens/CreationStack/AssignementCreationScreen.js
+++ src/screens/CreationStack/AssignementCreationScreen.js
@@ -1,29 +1,99 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import { Button } from "react-native-elements";
+import AssignementDetails from "../../Components/AssignementDetails";
+import { FontAwesome } from "@expo/vector-icons";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 const AssignementCreationScreen = () => {
   return (
-    <SafeAreaView style={styles.mainContainer}>
-      <View style={{ alignItems: "center", width: windowWidth * 0.6 }}>
+    <ScrollView>
+      <SafeAreaView style={styles.mainContainer}>
+        <View style={{ alignItems: "center", width: windowWidth * 0.6 }}>
+          <Text
+            style={{
+              fontSize: 20,
+              color: "#3A62BF",
+              fontWeight: "bold",
+              marginBottom: 10,
+            }}
+          >
+            CHEM 130
+          </Text>
+          <Text style={{ textAlign: "center" }}>
+            Enter all the following information regarding this course
+          </Text>
+        </View>
+        {/* Assignement Details Component */}
         <Text
           style={{
-            fontSize: 20,
-            color: "#3A62BF",
+            fontSize: 18,
             fontWeight: "bold",
-            marginBottom: 10,
+            color: "#3A62BF",
+            alignSelf: "flex-start",
+            marginLeft: 20,
+            marginVertical: 10,
           }}
         >
-          CHEM 130
+          Assignments/Homework
         </Text>
-        <Text style={{ textAlign: "center" }}>
-          Enter all the following information regarding this course
+        <AssignementDetails />
+
+        <Button
+          icon={<FontAwesome name="plus" size={24} color="#3A62BF" />}
+          buttonStyle={{
+            borderRadius: 50,
+            backgroundColor: "white",
+            height: 50,
+            width: 50,
+            marginVertical: 10,
+          }}
+        />
+
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            color: "#3A62BF",
+            alignSelf: "flex-start",
+            marginLeft: 20,
+            marginVertical: 10,
+          }}
+        >
+          Exams/Quizzes
         </Text>
-      </View>
-      {/* Assignement Details Component */}
-    </SafeAreaView>
+        <AssignementDetails />
+        <Button
+          icon={<FontAwesome name="plus" size={24} color="#3A62BF" />}
+          buttonStyle={{
+            borderRadius: 50,
+            backgroundColor: "white",
+            height: 50,
+            width: 50,
+            marginVertical: 10,
+          }}
+        />
+        <Button
+          title="Continue"
+          buttonStyle={{
+            borderRadius: 40,
+            backgroundColor: "#3A62BF",
+            height: 45,
+            width: 200,
+            marginVertical: 10,
+          }}
+        />
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
